5036cc7464de1427b1918407ce53b7a6ca4fe547
Joe Bell <joeaintexas@gmail.com>
2013-03-08 21:23:44 -0600

    First commit

---
+++ webservice.rb
@@ -0,0 +1,14 @@
+require 'sinatra'
+require 'json'
+
+get '/geofence/:fence_id/crossed' do
+  fence_id = params[:fence_id]
+  user_id  = params[:user_id]
+
+  puts "Geofence #{fence_id} has been crossed by #{user_id}"
+
+  status 200
+  return {status:  "ok",
+          crossing:  {geofence_id:  fence_id,
+                      user_id:      user_id}}.to_json
+end
